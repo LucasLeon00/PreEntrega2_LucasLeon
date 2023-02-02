34e5d22fda4b38cd28c86858755f238ab7fec7fa
LucasLeon00 <lucasmateoleon00@gmail.com>
2023-02-01 22:01:15 -0300

    Se actualiza el proyecto

--- Pages/index.js
+++ Pages/index.js
@@ -1,53 +1,28 @@
-alert("Ingrese la opcion del producto que desea llevar. Para salir ingrese 0")
-let seleccionarProductos = Number(prompt("1-Lana 100 g $150 2-Ajugas de crochet $250 c/u 3-Tejidos a crochet $3400 4-Amigurumis $2800"))
-
-let seleccionarCantidad;
-
-let total = 0;
-
-const cantidad = (cant, precio) => {
-    return cant*precio
-}
+const items = document.getElementById('items')
+const templateCard = document.getElementById('template-card').content 
+const fragment = document.createDocumentFragment()
 
 
+document.addEventListener('DOMContentLoaded',()=>{
+    fetchData()
+})
 
-while(seleccionarProductos != 0) {
-    switch(seleccionarProductos){
-        case 1:
-            seleccionarCantidad = Number(prompt("el producto es Lana 100g. Indique la cantidad"))
-            
-            total += cantidad(seleccionarCantidad, 150)
-        break;
-        case 2:
-            seleccionarCantidad = Number(prompt("el producto es Ajugas de crochet. Indique la cantidad"))
-            
-            total += cantidad(seleccionarCantidad, 250)
-        break;
-        case 3:
-            seleccionarCantidad = Number(prompt("el producto es Tejidos a crochet. Indique la cantidad"))
-            
-            total += cantidad(seleccionarCantidad, 3400)
-        break;
-        case 4:
-            seleccionarCantidad = Number(prompt("el producto es Amigurumis. Indique la cantidad"))
-           
-            total += cantidad(seleccionarCantidad, 2800)
-        break;
-        default:
-            break;
-    }
-    seleccionarProductos = Number(prompt("1-Lana 100 g $150 2-Ajugas de crochet $250 c/u 3-Tejidos a crochet $3400 4-Amigurumis $2800"))
-}
-
-alert("El total de la compra es : " + total)
-
-
-const envio = () =>{
-    if (total >= 3000){
-        alert("El envio es gratuito")
-    }else{
-        total += 780
-        alert("El costo de envio es de $780, el total es: " + total)
+const fetchData = async () => {
+    try{
+        const res = await fetch ('api.json')
+        const data = await res.json()
+        //console.log(data)
+    } catch (error) {
+        console.log(error)
     }
 }
-envio()
+
+const pintarCards = () => {
+    data.forEach(producto => {
+        templateCard.querySelector('h5').textContent = producto.title
+        
+        const clone = templateCard.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+    items.appendChild(fragment)
+}
